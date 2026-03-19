6805a5958544c844e2570df39eeb362584414a11
Piranha <nanaroselynn@gmail.com>
2026-03-19 18:22:02 +0100

    Add docker + nginx setup with frontend and backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5014", {
+const socket = io("/", {
   transports: ["websocket", "polling"],
 });
 
